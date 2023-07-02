22788e5113ddd2ab008f95f4bd86226b8cef4dd9
Robert Lee <robertlee@Roberts-MBP.attlocal.net>
2023-07-02 01:11:13 -0700

    added all html

---
+++ index.js
@@ -0,0 +1,126 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: "What is the manager name?"
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: "What is the manager employee id?",
+    },
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: "What is the manager email address?",
+    },
+    {
+        type: 'input',
+        name: 'managerOffice',
+        message: "What is the manager office number?",
+    },
+];
+
+const employeeQuestions = [
+    {
+        type: 'list',
+        name: 'employeeType',
+        message: "Add another employee",
+        choices: ['Engineer', 'Intern', 'Done'],
+    },
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: "What is the employee's name?",
+        when: (answers) => answers.employeeType !== 'Done'
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: "What is the employee's id?",
+        when: (answers) => answers.employeeType !== 'Done'
+    },
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: "What is the employee's email?",
+        when: (answers) => answers.employeeType !== 'Done'
+    },
+    {
+        type: 'input',
+        name: 'employeeGithub',
+        message: "What is the employee's github?",
+        when: (answers) => answers.employeeType === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'employeeSchool',
+        message: "What school did the intern attend?",
+        when: (answers) => answers.employeeType === 'Intern'
+    }
+];
+
+const questionAnswers = [];
+
+async function promptManagerQuestions() {
+    try {
+        const answers = await inquirer.prompt(managerQuestions);
+        questionAnswers.push(new Manager(answers.employeeName, answers.managerId, answers.employeeEmail, answers.managerOffice));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function promptEmployeeQuestions() {
+    const answers = await inquirer.prompt(employeeQuestions);
+    return answers;
+}
+
+
+async function init() {
+    await promptManagerQuestions();
+
+    let contAsking = true;
+    while(contAsking) {
+        const answers = await promptEmployeeQuestions();
+
+        if(await answers.employeeType !== 'Done') {
+            if(answers.employeeType === 'Engineer') {
+                questionAnswers.push(new Engineer(
+                    answers.employeeName, 
+                    answers.employeeId, 
+                    answers.employeeEmail, 
+                    answers.employeeGithub));
+            }
+            if(answers.employeeType === 'Intern') {
+                questionAnswers.push(new Intern(
+                    answers.employeeName, 
+                    answers.employeeId, 
+                    answers.employeeEmail, 
+                    answers.employeeSchool));
+            }
+        } else {
+            contAsking = false;
+        }
+    }
+    const HTMLContent = generateHTML(questionAnswers);
+    writeToFile('./dist/index.html', HTMLContent);
+
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Your team profile has been updated')
+        );
+    };
+    
+};
+
+// Function call to initialize app
+init();
---
+++ src/generateHTML.js
@@ -0,0 +1,68 @@
+function generateCards(data) {
+    let cardsInfo = "";
+
+    for(let i = 0; i < data.length; i++) {
+        cardsInfo += `<div class='card'>
+                        <h2>
+                            ${data[i].getName()}
+                            <br>
+                            ${data[i].getRole() === 'Manager' ? '<i class="fa-solid fa-mug-hot"></i>'+data[i].getRole() : ''}
+                            </br>
+
+                            <br>
+                            ${data[i].getRole() === 'Engineer' ? '<i class="fa-solid fa-glasses"></i>'+data[i].getRole() : ''}
+                            </br>
+                        
+                            <br>
+                            ${data[i].getRole() === 'Intern' ? '<i class="fa-solid fa-user-graduate"></i>'+data[i].getRole() : ''}
+                            </br>
+                        </h2>
+                            <div class='card-content'>
+                                <p>
+                                    ID: ${data[i].getId()}
+                                </p>
+                                <p>
+                                    Email: <a href="mailto:${data[i].getEmail()}">${data[i].getEmail()}</a>
+                                </p>
+                                
+                                <br>
+                                ${data[i].getRole() === 'Manager' ? '<p>Office number: '+data[i].getOfficeNumber()+'</p>' : ''}
+                                </br>
+
+                                <br>
+                                ${data[i].getRole() === 'Engineer' ? '<p>Github: <a href="https://github.com/'+data[i].getGithub()+'">'+ data[i].getGithub() +'</a>'+'</p>' : ''}
+                                </br>
+
+                                <br>
+                                ${data[i].getRole() === 'Intern' ? '<p>School: '+data[i].getSchool()+'</p>' : ''}
+                                </br>
+                            </div>
+                        </div>`;
+    }
+    return cardsInfo;
+}
+
+function generateHTML(data){
+
+    return`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" type="text/css" href="styles.css" /> 
+            <title>Document</title>
+        </head>
+        <header>My Team</header>
+        <body>
+            <div class='container'>
+                ${generateCards(data)}
+            </div>
+        </body>
+        </html>
+    `;
+}
+
+module.exports = generateHTML;
+
